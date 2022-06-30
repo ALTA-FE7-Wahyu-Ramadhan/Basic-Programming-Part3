6734ac89a53767386d1bd9c685342f03557853f0
unknown <wahyuramadhan0549@gmail.com>
2022-07-01 01:41:27 +0700

    problem3 was fixed

--- Part5/problem3/problem3.js
+++ Part5/problem3/problem3.js
@@ -1,5 +1,19 @@
 function arrayUnique(arrayA, arrayB) {
   // Your Code Here
+  let result = [];
+  for (let i = 0; i < arrayA.length; i++) {
+    let double = 0;
+    for (let j = 0; j < arrayA.length; j++) {
+      if (arrayA[i] == arrayB[j]) {
+        double += 1;
+      }
+    }
+    if (double == 0) {
+      result.push(arrayA[i]);
+    }
+    double = 0;
+  }
+  return result;
 }
 
 console.log(arrayUnique([1, 2, 3, 4], [1, 3, 5, 10, 16])); // [2, 4]
